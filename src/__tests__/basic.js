b4af3c3361613dd975fcb7a73ed9b4d3b373d768
Vasilij Golnev <vasilij.golnev@yandex.ru>
2022-04-13 03:59:04 +0700

    add in ajs-4

---
+++ src/__tests__/basic.js
@@ -0,0 +1,46 @@
+import calculateTotal from '../calculate.js';
+
+test('basic test', () => {
+    const result = 34;
+    expect(result).toBe(34);
+});
+
+test('calculateTotal sum without discount', () => {
+    const list = [
+        {
+            id: 475,
+            name: 'Психология',
+            count: 1,
+            price: 1200
+        },
+        {
+            id: 476,
+            name: 'Логика',
+            count: 3,
+            price: 600
+        }
+    ];
+    let result = calculateTotal(list);
+
+    expect(result).toBe(3000);
+});
+
+test('calculateTotal sum with discount', () => {
+    const list = [
+        {
+            id: 475,
+            name: 'Психология',
+            count: 1,
+            price: 1200
+        },
+        {
+            id: 476,
+            name: 'Логика',
+            count: 3,
+            price: 601
+        }
+    ];
+    let result = calculateTotal(list, true);
+
+    expect(result).toBe(1531.53);
+});
---
+++ src/calculate.js
@@ -0,0 +1,12 @@
+export default function calculateTotal(purchases, applyDiscount) {
+    
+    const sum = purchases.reduce((total, item) => {
+        return total + (item.count * item.price);
+    }, 0);
+
+    if (applyDiscount) {
+        return sum * 0.51;
+    }
+
+    return sum;
+}
